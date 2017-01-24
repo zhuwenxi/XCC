12cc74dcfeea158b45725de40896b6a07638dd98
wenxizhu <zhuwenxi8935@gmail.com>
2017-01-24 13:16:55 +0800

    Improve the "src/script/stat.sh" 1. Fix a bug: the error relative file paths; 2. Now *.sh files are also count as source files.

--- src/script/stat.sh
+++ src/script/stat.sh
@@ -2,31 +2,42 @@
 
 total_line_nmbr=0
 
+current_parent="."
+current_parent_deps=0
+
 visit_dir() {
 	parent_dir=$1
 	pushd $parent_dir > /dev/null
 
+	if [ $parent_dir != "." ]; then
+		current_parent_deps=$((current_parent_deps+1))
+		current_parent=$current_parent/$1
+	fi
+
 	sub_dirs=$(ls)
-	sub_files="$(ls | egrep "*\.c$") $(ls | egrep "*\.h$") $(ls | egrep "*\.def")"
+	sub_files="$(ls | egrep "*\.c$") $(ls | egrep "*\.h$") $(ls | egrep "*\.def") $(ls | egrep "*\.sh$")" 
 
 	for file in $sub_files; do
 		line_nmbr=$(wc -l $file | cut -d " " -f1)
 		total_line_nmbr=$((total_line_nmbr+line_nmbr))
-		printf "   %-4d lines : %s\n" $line_nmbr $1/$file
+		printf "   %-4d lines : %s\n" $line_nmbr $current_parent/$file
 	done
 
 	for dir in $sub_dirs
 	do
 		if [ -d $dir ]; then 
 			visit_dir $dir
-		# elif [ -f $dir]; then
-			# line_nmbr = $(wc -l $dir)
-			# total_line_nmbr=$((total_line_nmbr+line_number))
-			# echo "count lines in " $dir": " $line_numbr 
 		fi
 	done
 
 	popd > /dev/null
+	
+	last_but_not_least=$((current_parent_deps))
+
+	if [ $current_parent_deps -gt 0 ]; then
+		current_parent=$(echo $current_parent | cut -d "/" -f 1-$last_but_not_least)
+		current_parent_deps=$((current_parent_deps-1));
+	fi
 }
 
 printf "==================================================\n\n"
