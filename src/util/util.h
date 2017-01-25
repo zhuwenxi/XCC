6e0968bab29a20f0bfa32569177fa78e1eeb6706
wenxizhu <zhuwenxi8935@gmail.com>
2017-01-25 23:33:06 +0800

    Start to implement the linked list.

--- src/util/util.h
+++ src/util/util.h
@@ -23,9 +23,51 @@ do{\
 #define DBCODE(flag, code)
 #endif
 
+
+
+
+
 //
 // Boolean type: true & false.
 //
 #define true 1
 #define false 0
+
+
+
+
+//
+// Bi-direction linked list.
+//
+typedef struct linked_list_node
+{
+	void *data;
+	struct linked_list_node *next;
+	struct linked_list_node *prev;
+} linked_list_node_t;
+
+typedef struct 
+{
+	linked_list_node_t *head;
+	linked_list_node_t *tail;
+} linked_list_t;
+
+linked_list_node_t *
+linked_list_first_item(linked_list_t *list);
+
+linked_list_node_t *
+linked_list_last_item(linked_list_t *list);
+
+linked_list_node_t *
+linked_list_limit(linked_list_t *list);
+
+linked_list_node_t *
+linked_list_next(linked_list_node_t *node);
+
+linked_list_node_t *
+linked_list_prev(linked_list_node_t *node);
+
+#define LL_NODE_DATA(node, type) ((type *)(node->data)) 
+
+
 #endif
