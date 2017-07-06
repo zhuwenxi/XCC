b582583b79c80bf9b57e2c417a00c92977cdf434
wenxizhu <wenxi.zhu@intel.com>
2017-07-06 16:55:12 +0800

    Implementing context_free_grammar.

--- include/util/context_free_grammar.h
+++ include/util/context_free_grammar.h
@@ -23,6 +23,10 @@ bool context_free_grammar_destroy(context_free_grammar_type *grammar, ...);
 
 bool context_free_grammar_deconstructor(context_free_grammar_type *grammar, va_list arg_list);
 
+production_type *production_create();
+
+bool production_deconstructor(production_type *prod, va_list arg_list);
+
 void context_free_grammar_add(context_free_grammar_type *grammar, char *head, ...);
 
 #endif
--- src/util/context_free_grammar.c
+++ src/util/context_free_grammar.c
@@ -1,1 +1,82 @@
 #include "context_free_grammar.h"
+
+context_free_grammar_type *
+context_free_grammar_create()
+{
+	context_free_grammar_type *grammar = (context_free_grammar_type *)malloc(sizeof(context_free_grammar_type));
+	grammar->productions = array_list_create();
+
+	return grammar;
+}
+
+bool
+context_free_grammar_destroy(context_free_grammar_type *grammar, ...)
+{
+	if (grammar == NULL)
+	{
+		return FALSE;
+	}
+
+	va_list arg_list;
+	va_arg(arg_list, grammar);
+	context_free_grammar_deconstructor(grammar, arg_list);
+	va_end(arg_list);
+
+	return TRUE;
+}
+
+bool
+context_free_grammar_deconstructor(context_free_grammar_type *grammar, va_list arg_list)
+{
+	if (grammar == NULL)
+	{
+		return FALSE;
+	}
+
+	array_list_deconstructor(grammar->productions, arg_list);
+
+	free(grammar);
+
+	return FALSE;
+}
+
+production_type *
+production_create()
+{
+	production_type *production = (production_type *)malloc(sizeof(production_type *));
+
+	production->head = NULL;
+	production->body = linked_list_create();
+
+	return production;
+}
+
+bool
+production_deconstructor(production_type *prod, va_list arg_list)
+{
+	if (prod == NULL)
+	{
+		return FALSE;
+	}
+
+	linked_list_destroy(prod->body, arg_list);
+
+	free(prod);
+
+	return FALSE;
+}
+
+
+void
+context_free_grammar_add(context_free_grammar_type *grammar, char *head, ...)
+{
+	assert(grammar != NULL && head != NULL);
+
+	production_type *production = production_create();
+	production->head = head;
+
+	
+
+	array_list_append(array_list_grammar->productions, head);
+}
+
